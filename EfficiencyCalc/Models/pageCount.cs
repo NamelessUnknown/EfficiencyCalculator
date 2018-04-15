2225cc3c3b8db867953b8b57e5e25adc94f306ed
Lukasz Debiec <lukasz.r.debiec@gmail.com>
2018-04-15 20:22:33 +0200

    Basic classes

---
+++ EfficiencyCalc/Models/pageCount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EfficiencyCalc.Models
+{
+    public class PageCount
+    {
+        public string worker { get; set; }
+        public int inspectedPages { get; set; }
+        public int associations { get; set; }
+        public int ads { get; set; }
+        public int timeSpentOnAds { get; set; }
+        public Department department { get; set; }
+        public Shift shift { get; set; }
+    }
+}
