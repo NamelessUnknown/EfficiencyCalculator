84b96c4e41056a7773e63eeee937c9719f1a85b8
Lukasz Debiec <lukasz.r.debiec@gmail.com>
2018-04-15 21:18:12 +0200

    Database connection

---
+++ EfficiencyCalc/DAL/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EfficiencyCalc.DAL
+{
+    public class DatabaseContext : IdentityDbContext
+    {
+        public DatabaseContext (DbContextOptions<DatabaseContext> options) : base (options) { }
+    }
+}
--- EfficiencyCalc/Startup.cs
+++ EfficiencyCalc/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using EfficiencyCalc.DAL;
+using Microsoft.EntityFrameworkCore;
 
 namespace EfficiencyCalc
 {
@@ -23,6 +25,10 @@ namespace EfficiencyCalc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            var dbConnectionString = @"Server=localhost\SQLEXPRESS;Database=EfficiencyCalc;Trusted_Connection=True;";
+
+            services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(dbConnectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
